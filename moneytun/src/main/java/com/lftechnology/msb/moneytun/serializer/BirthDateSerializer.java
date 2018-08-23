d6e620595baea77b3498eca52d9b1fcf5e8137d6
adhpawal <pawaladhikari@lftechnology.com>
2018-08-23 09:15:31 +0545

    DOB Changes

---
+++ moneytun/src/main/java/com/lftechnology/msb/moneytun/serializer/BirthDateSerializer.java
@@ -0,0 +1,21 @@
+package com.lftechnology.msb.moneytun.serializer;
+
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateSerializer extends JsonSerializer<LocalDate> {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    
+    @Override
+    public void serialize(LocalDate localDate, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String dateString = localDate.format(formatter);
+        jsonGenerator.writeString(dateString);
+    }
+}

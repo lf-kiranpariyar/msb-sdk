24059682a50c9568d212fed6821c40a047ef79d3
RaskinMaharjan <raskin0maharjan@gmail.com>
2018-09-24 14:02:37 +0545

    #MPR-2576 : add Account and Balance dto

--- vtnsdk/src/main/java/com/lftechnology/vtn/sdk/dto/Account.java
+++ vtnsdk/src/main/java/com/lftechnology/vtn/sdk/dto/Account.java
@@ -4,11 +4,20 @@ import lombok.Data;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.math.BigDecimal;
+import java.util.Date;
 
-@XmlRootElement(name="RemittanceBalanceResponse")
 @Data
-public class Account extends Response {
-    @XmlElement(name ="AccountBalance")
-    private BigDecimal balance;
+@XmlRootElement(name="RemittanceAccountStatusResponse")
+public class Account extends Response{
+    @XmlElement(name="FirstName")
+    private String firstName;
+    @XmlElement(name="LastName")
+    private String lastName;
+    @XmlElement(name="Email")
+    private String email;
+    @XmlElement(name="Phone")
+    private String phone;
+    @XmlElement(name = "DateOfBirth")
+    private Date dob;
+
 }
---
+++ vtnsdk/src/main/java/com/lftechnology/vtn/sdk/dto/Balance.java
@@ -0,0 +1,14 @@
+package com.lftechnology.vtn.sdk.dto;
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.math.BigDecimal;
+
+@XmlRootElement(name="RemittanceBalanceResponse")
+@Data
+public class Balance extends Response {
+    @XmlElement(name ="AccountBalance")
+    private BigDecimal balance;
+}

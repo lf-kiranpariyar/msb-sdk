8c87c9727a4f7cf702a0459560a39b44ed649b28
shankar <coder.shankar.com>
2018-10-05 14:51:38 +0545

    vtn integration on gateway

--- vtn/src/test/java/com/lftechnology/vtn/api/BankApiTest.java
+++ vtn/src/test/java/com/lftechnology/vtn/api/BankApiTest.java
@@ -5,16 +5,17 @@ import com.lftechnology.vtn.dto.request.Credential;
 import com.lftechnology.vtn.dto.response.BankResponse;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class BankApiTest {
-    private Credential credential = new Credential("LKNgtr4Of7VmHrr7Q8JJcgbt543sFB4","MNV73Bc6655dJ8UdkG4IKNGyk82nONK4");
+    Credential credential = new Credential("Invalid", "invalid");
+
     @Test
     public void verifyBank() {
         //Given
         BankApi bankApi = new BankApi(credential);
         BankRequest bankRequest = new BankRequest();
-        bankRequest.setBankAccountName("PETER   OJO");
+        bankRequest.setBankAccountName("PETER OJO");
         bankRequest.setBankAccountNumber("2173986879");
         bankRequest.setBankName("000015");
 
